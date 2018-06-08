1d413ebb166d8c102f7173a3ca995c44eb5312f5
Kenno <33248162+Stotex@users.noreply.github.com>
2018-06-08 05:06:32 +0300

    Commit

---
+++ Assets/Scripts/Mace.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mace : MonoBehaviour {
+
+    float speed;
+    Vector3 direction;
+    float min;
+    float max;
+    float units = 2.0f;
+
+    void Start()
+    {
+        max = transform.position.y;
+        min = transform.position.y - units;
+
+        direction = Vector3.down;
+    }
+
+    void Update()
+    {
+
+        if (direction == Vector3.down)
+        {
+            speed = 10.0f;
+        }
+
+        else if (direction == Vector3.up)
+        {
+            speed = 1.0f;
+        }
+
+
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (transform.position.y >= max)
+        {
+            direction = Vector3.down;
+        }
+
+        if (transform.position.y <= min)
+        {
+            direction = Vector3.up;
+        }
+    }
+}
---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+
+    private Player player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void onTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
